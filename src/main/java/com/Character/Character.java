f98597531ce6facd115cb833c42b9b27e862173d
Ramya Sivamani <ramya.s@KGFSL.COM>
2017-08-17 19:43:07 +0530

    test

---
+++ src/main/java/com/Character/Character.java
@@ -0,0 +1,44 @@
+package com.Character;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Character {
+
+    @Column(name = "name")
+    private String name;
+
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
+    private Integer id;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	
+	public Character() {
+		
+	}
+
+	
+    // getters, setters, etc, omitted
+    
+}
